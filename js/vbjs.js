4da13141af4815dbca9daa4dacd8e8ae7928c65c
Minh Lam <Lammn1990@gmail.com>
2015-11-20 01:15:56 -0800

    slider with nav.
    semi carousel without autoplay.  need more tweak on dots

--- js/vbjs.js
+++ js/vbjs.js
@@ -1,22 +1,61 @@
 var main = function(){
 
+	
+	$('.arrow-next').click(function(){
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide.next()
+
+		var currentDot = $('.active-dot');
+		var nextDot = currentDot.next()
+
+		if (nextSlide.length === 0) {
+			nextSlide = $('.slide').first();
+			nextDot = $('.dot').first();
+		}
+
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		nextSlide.fadeIn(600).addClass('active-slide');
+
+		currentDot.removeClass('active-dot');
+		nextDot.addClass('active-dot');
+	});
+
+	$('.arrow-prev').click(function(){
+		var currentSlide = $('.active-slide');
+		var prevSlide = currentSlide.prev();
+
+		var currentDot = $('.active-dot');
+		var prevDot = currentDot.prev();
+		if(prevSlide.length === 0) {
+     	 	prevSlide = $('.slide').last();
+     		prevDot = $('.dot').last();
+     	}
+     	currentSlide.fadeOut(600).removeClass('active-slide');
+     	prevSlide.fadeIn(600).removeClass('active-slide');
+
+     	currentDot.removeClass('active-dot');
+     	prevDot.addClass('active-dot');
+	});
+
+	
+
+
 	$('.dot1').click(function(){
 		$('.slide').hide();
-		$('.slide1').show();
+		$('#slide1').show();
 	});
 	$('.dot2').click(function(){
-		$('.slide').fadeOut('fast');
-		$('.slide2').show();
-		$('.slide2').fadeIn('slow', 2000);
+		$('.slide').hide();
+		$('#slide2').show();
 	});
 	$('.dot3').click(function(){
 		$('.slide').hide();
-		$('.slide3').show();
+		$('#slide3').show();
 	});
 	$('.dot4').click(function(){
 		$('.slide').hide();
-		$('.slide4').show();
-	});
+		$('#slide4').show();
+	}); 
 };
 
 function displayTime () {
