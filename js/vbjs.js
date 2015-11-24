d10c08c66d1d2577c373dce111761fe49a42756e
Minh Lam <Lammn1990@gmail.com>
2015-11-24 12:55:51 -0800

    add resume page.  slide navigation done
    finish with main page navigation.  Still need to make design more
    aesthetically pleasing

--- js/vbjs.js
+++ js/vbjs.js
@@ -1,20 +1,27 @@
+function displayTime () {
+	var elt = document.getElementById("clock");
+	var now = new Date();
+	elt.innerHTML = now.toLocaleTimeString();
+	setTimeout (displayTime, 1000);
+};
+displayTime();
+
 var main = function(){
 
-	
 	$('.arrow-next').click(function(){
 		var currentSlide = $('.active-slide');
-		var nextSlide = currentSlide.next()
+		var nextSlide = currentSlide.next();
 
 		var currentDot = $('.active-dot');
-		var nextDot = currentDot.next()
+		var nextDot = currentDot.next();
 
 		if (nextSlide.length === 0) {
 			nextSlide = $('.slide').first();
 			nextDot = $('.dot').first();
 		}
 
-		currentSlide.fadeOut(600).removeClass('active-slide');
-		nextSlide.fadeIn(600).addClass('active-slide');
+		currentSlide.fadeOut(500).removeClass('active-slide');
+		nextSlide.fadeIn(500).addClass('active-slide');
 
 		currentDot.removeClass('active-dot');
 		nextDot.addClass('active-dot');
@@ -26,45 +33,31 @@ var main = function(){
 
 		var currentDot = $('.active-dot');
 		var prevDot = currentDot.prev();
+
 		if(prevSlide.length === 0) {
      	 	prevSlide = $('.slide').last();
      		prevDot = $('.dot').last();
      	}
-     	currentSlide.fadeOut(600).removeClass('active-slide');
-     	prevSlide.fadeIn(600).removeClass('active-slide');
+     	currentSlide.fadeOut(500).removeClass('active-slide');
+     	prevSlide.fadeIn(500).addClass('active-slide');
 
      	currentDot.removeClass('active-dot');
      	prevDot.addClass('active-dot');
 	});
 
+
+	$('.dot').click(function(){
+		var index = $(this).index(); // get the index or position of the current element that has the class .dot
+		$('.slide').fadeOut(500); // to hide all elements with class .slide
+		$('.dot').removeClass('active-dot');
+		$('.slide').removeClass('active-slide').addClass('active');
+		$('#slide' + (index+1)).fadeIn(500); 
+		$('#slide' + (index+1)).removeClass('active').addClass('active-slide');
+		$(this).addClass('active-dot');
+		});
 	
-
-
-	$('.dot1').click(function(){
-		$('.slide').hide();
-		$('#slide1').show();
-	});
-	$('.dot2').click(function(){
-		$('.slide').hide();
-		$('#slide2').show();
-	});
-	$('.dot3').click(function(){
-		$('.slide').hide();
-		$('#slide3').show();
-	});
-	$('.dot4').click(function(){
-		$('.slide').hide();
-		$('#slide4').show();
-	}); 
 };
 
-function displayTime () {
-	var elt = document.getElementById("clock");
-	var now = new Date();
-	elt.innerHTML = now.toLocaleTimeString();
-	setTimeout (displayTime, 1000);
-};
-displayTime();
 
 $(document).ready(main);
 
